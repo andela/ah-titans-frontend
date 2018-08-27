2da5717fbfe6df2fedcce60af1eeee1ec5bf754b
Janet Wairimu <janetnim4012gmail.com>
2018-08-27 17:44:09 +0300

    [Chore #160011168] Setting up configurations for development and production

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,9 @@
-import React, { Component } from "react";
-import { hot } from "react-hot-loader";
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+);
 
-export default hot(module)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+ReactDOM.render(<App />, document.getElementById('root'));
