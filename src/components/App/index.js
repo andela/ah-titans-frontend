d898430a206de65adfb422df2a2be9f0e73bfcb3
Janet Wairimu <janetnim4012gmail.com>
2018-08-28 13:23:59 +0300

    [Chore #160058777] Change the applications folder structure

---
+++ src/components/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Loader from '../Loader';
+
+const App = () => (
+  <div align="center">
+    <Loader />
+    <h1>Welcome to Authors Haven</h1>
+  </div>
+);
+
+export default App;
