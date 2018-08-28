bd7a9cf9e8afd1e17aacd25f81b6c29f45249c30
Janet Wairimu <janetnim4012gmail.com>
2018-08-28 13:22:35 +0300

    [Chore #160058777] Change path to App file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
