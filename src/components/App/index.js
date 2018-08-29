ce53f8c5105888533fd4b337c3a5aeb8309d81dd
Janet Nim <janetnim401@gmail.com>
2018-08-29 10:50:14 +0300

    #160058777 Deploy the Application to Heroku (#8)
    * [Chore #160058777] Change title to Authors Haven
    
    * [Chore #160058777] Add path to loader
    
    * [Chore #160058777] Change path to App file
    
    * [Chore #160058777] Add loader gif
    
    * [Chore #160058777] Change the applications folder structure
    
    * [Chore #160058777] Deploy app to heroku
    
    * [Chore #160058777] Delete yarn lock file
    
    * [Chore #160058777] Deploy app to heroku
    
    * [Chore #160058777] Add procfile
    
    * [Chore #160058777] Add procfile
    
    * [Chore #160058777] Add procfile
    
    * [Chore #160058777] Edit procfile
    
    * [Chore #160058777] Edit package script
    
    * Add End of Line
    
    * Add End of Line
    
    * [Chore #160058777] Edit test file
    
    * [Chore #160058777] Edit test file
    
    * [Chore #160058777] Delete prod line
    
    * [Chore #160058777] Add css/scss file extensions to transformer]
    
    * #160011174 Run codeclimate test coverage (#6)
    
    * [Chore #160011174] Update README.md
    
    * [Chore #160011174] Add CircleCI yml config
    
    * [Chore #160011174] Update CircleCI config
    
    * [Chore #160011174] Add Maintainability badge
    
    * [Chore #160011174] Update CircleCI configurations
    
    * [Chore #160011174] Add Test Coverage badge
    
    * [Chore #160011174] Remove comments from config
    
    * [Chore #160011174] Update reporter command
    
    * [Chore #160058777] Change title to Authors Haven
    
    * [Chore #160058777] Add path to loader
    
    * [Chore #160058777] Change path to App file
    
    * [Chore #160058777] Add loader gif
    
    * [Chore #160058777] Change the applications folder structure
    
    * [Chore #160058777] Deploy app to heroku
    
    * [Chore #160058777] Delete yarn lock file
    
    * [Chore #160058777] Deploy app to heroku
    
    * [Chore #160058777] Add procfile
    
    * [Chore #160058777] Add procfile
    
    * [Chore #160058777] Add procfile
    
    * [Chore #160058777] Edit procfile
    
    * [Chore #160058777] Edit package script
    
    * Add End of Line
    
    * Add End of Line
    
    * [Chore #160058777] Edit test file
    
    * [Chore #160058777] Edit test file
    
    * [Chore #160058777] Delete prod line
    
    * [Chore #160058777] Add css/scss file extensions to transformer]

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Loader from '../Loader';
+import './index.css';
+
+const App = () => (
+  <div className="app">
+    <Loader />
+    <h1>Welcome to Authors Haven</h1>
+  </div>
+);
+
+export default App;
--- tests/app/App.test.js
+++ tests/app/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
-import App from '../../src/App/index';
+import { shallow, } from 'enzyme';
+import App from '../../src/components/App';
 
 describe('App', () => {
   it('renders without crashing', () => {
