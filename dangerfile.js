bdbacb46f96c5a6ba0967f78e6aee346348789c8
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2018-08-29 15:35:03 +0300

    [Chore #160125624] Change Danger configurations

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,19 @@
-import { fail, warn, message, markdown, danger } from "danger"
+const jest = require('danger-plugin-jest').default;
+const { message, danger, warn, } = require('danger');
 
-fail("This is a failure message")
-warn("This is a warning")
-message("This is a normal message")
-markdown("*Markdown* is also **supported**")
+// Output all modified files in the PR
+const modified = danger.git.modified_files;
+message(`Changed Files in this PR: \n${modified.map(file => `- ${file}\n`)}`);
 
-const { additions = 0, deletions = 0 } = danger.github.pr
-message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
+// Encourage smaller PRs
+let errorCount = 0;
+const bigPRThreshold = 600;
+const thresholdErrorMessage = 'Pull Request size seems quite large. If the PR contains multiple changes, try splitting each change into a separate PR to aid faster, easier reviews.';
+if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+  warn(
+    `:exclamation: PR is too big (${++errorCount}) - ${thresholdErrorMessage}`
+  );
+}
+
+// Run jest test suites
+jest();
