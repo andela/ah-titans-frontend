c173c6badcbb18c3c6f7e21142f4d74ec72cdcd7
chepmanu <emmanuelchepsire33@gmail.com>
2018-08-29 15:14:16 +0300

    [Chore #160011172 ] Added react-router-dom and changed file structure

--- src/App/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => (
-  <div className="app">
-    <h1>Hello World</h1>
-  </div>
-);
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, } from 'react-redux';
-import App from './components/App';
+import App from './views/App';
 import store from './redux/store';
+import { BrowserRouter, } from 'react-router-dom';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+  </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
