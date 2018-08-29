e1b429f72f937c4fc4f5524ecd98bac5b502175d
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2018-08-29 17:44:13 +0300

    [Chore #160125624] Refactor cusom modifiers

--- dangerfile.js
+++ dangerfile.js
@@ -46,23 +46,25 @@ const testFilesThatDontExist = correspondingTestsForModifiedFiles
   .filter(f => !f.includes('__stories__')) // skip stories
   .filter(f => !fs.existsSync(f));
 
-if (testFilesThatDontExist.length > 0 && !skipTests) {
-  const output = `Missing Test Files:
-${testFilesThatDontExist.map(f => `- \`${f}\``).join('\n')}
-If these files are supposed to not exist, please update your PR body to include "Skip New Tests".`;
-  warn(output);
-}
-
 // Find changed React components
 const changedComponents = modifiedFiles.filter((file) => {
   const content = fs.readFileSync(file).toString();
   return content.includes('React');
 });
 
+function customModifiers(modifier, messageTitle, messageDescription) {
+  const result = `${messageTitle}:
+  ${modifier.map(f => `- \`${f}\``).join('\n')}
+  ${messageDescription}`;
+  warn(result);
+}
+if (testFilesThatDontExist.length > 0 && !skipTests) {
+  customModifiers(testFilesThatDontExist, 'Missing Test Files',
+    'If these files are supposed to not exist, please update your PR body to include "Skip New Tests".');
+}
+
 // Check for images in PR description if new components added
 if (changedComponents.length > 0 && !skipVisualDiff && !hasScreenShots) {
-  const output = `Should there be images to represent these components:
-  ${changedComponents.map(f => `- \`${f}\``).join('\n')}
-  If these changes are not visual, please update your PR body to include "Skip Visual Diff".`;
-  warn(output);
+  customModifiers(changedComponents, 'Should there be images to represent these components',
+    'If these changes are not visual, please update your PR body to include "Skip Visual Diff".');
 }
