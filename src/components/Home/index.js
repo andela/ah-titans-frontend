7953af2237ab417ed67a51a8a1e97f5ed7e93a08
chepmanu <emmanuelchepsire33@gmail.com>
2018-08-29 19:50:00 +0300

    #160011172  Setup react router  (#11)
    * [Chore #160011172 ] Added react-router-dom and changed file structure
    
    * [Chore #160011172 ] Changed file structure
    
    * [Chore #160011172 ] Resolved importation errors
    
    * [Chore #160011172 ] Removed unnecessary comments
    
    * [Chore #160011172 ] Added tests

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Loader from '../../views/Loader';
+
+import './index.scss';
+
+const Home = () => (
+  <div className="app">
+    <Loader />
+    <h1>Welcome to Authors  Haven!</h1>
+  </div>
+);
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, } from 'react-redux';
-import App from './components/App';
+import { BrowserRouter, } from 'react-router-dom';
+import App from './views/App';
 import store from './redux/store';
+
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
