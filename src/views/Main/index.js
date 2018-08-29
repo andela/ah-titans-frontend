3832d802523628864cf828d7b7514f111a0e6309
chepmanu <emmanuelchepsire33@gmail.com>
2018-08-29 17:03:56 +0300

    [Chore #160011172 ] Removed unnecessary comments

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -3,11 +3,6 @@ import { Switch, Route, } from 'react-router-dom';
 import Home from '../../components/Home';
 import Login from '../../components/Login';
 
-// The Main component renders one of the three provided
-// Routes (provided that one matches). Both the /roster
-// and /schedule routes will match any pathname that starts
-// with /roster or /schedule. The / route will only match
-// when the pathname is exactly the string "/"
 const NotFound = () => <h1>Not found</h1>;
 
 const Main = () => (
