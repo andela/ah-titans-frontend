1fad2073a750c3ef1dca45a8b2316674ef6f87b7
petermwash <petermwash48@gmail.com>
2018-08-30 17:10:41 +0300

    [Chore #160088425] Refactor code

--- src/Signup/Signup.test.js
+++ src/Signup/Signup.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { shallow, } from 'enzyme';
+import { shallow, mount} from 'enzyme';
 
 import Signup from './containers/Signup';
+import SignupForm from './components/SignupForm';
 
 describe('User Signup', () => {
-  it('renders the form', () => {
+  it('renders the signup form', () => {
     const wrapper = shallow(<Signup />);
 
     expect(wrapper.find('SignupForm').length).toBe(1);
+
+  });
+  it('renders a form', () => {
+    const wrapper = shallow( <SignupForm /> );
+
+    expect(wrapper.find('form').length).toBe(1);
   });
 });
