7324319275a7a367af6b21c29a840be91ac8c744
petermwash <petermwash48@gmail.com>
2018-08-30 18:01:23 +0300

    [Chore #160088425] Refactor code

--- src/Signup/Signup.test.js
+++ src/Signup/Signup.test.js
@@ -5,15 +5,15 @@ import Signup from './containers/Signup';
 import SignupForm from './components/SignupForm';
 
 describe('User Signup', () => {
-  it('renders the signup form', () => {
-    const signupWrapper = shallow(<Signup />);
-
-    expect(signupWrapper.find('SignupForm').length).toBe(1);
+  it('renders the signup form succefully', () => {
+    shallow(<Signup />);
 
   });
-  it('returns a form', () => {
-    const signupFormWrapper = shallow( <SignupForm /> );
+});
 
+describe('Signup form', () => {
+  it('returns a form', () => {
+    const signupFormWrapper = shallow(<SignupForm />);
     expect(signupFormWrapper.find('form').length).toEqual(1);
   });
 });
