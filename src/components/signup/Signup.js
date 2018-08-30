0ef38edebd8cd1e24085f22f81b062799567ac95
petermwash <petermwash48@gmail.com>
2018-08-30 10:11:49 +0300

    [Feature #160088425] Configure Route for signup

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -4,48 +4,47 @@ import './Signup.css';
 const Logo = require('../../assets/logo.png');
 
 class Signup extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+    this.state = {
 
-    render(){
-        return(
-            <div className = 'container' >
-            <div className="card">
-                <div className = "box" id="sign-up">
-                <div className = "logo">
-                    <img src={Logo} alt="App logo"/>
-                </div>
-                <h2 className = "title" > Sign up now </h2>
+    };
+  }
+
+  render() {
+    return (
+      <div className="container flex">
+        <div className="card">
+          <div className="box" id="sign-up">
+            <div className="logo">
+              <img src={Logo} alt="App logo" />
             </div>
-            <div className="form ">
-                <form>
-                    <div>
-                        <input type="text" name="username" placeholder="Your username" required/>
-                    </div>
-                    <div>
-                        <input type="email" name="email" placeholder="Your email" required/>
-                    </div>
-                    <div>
-                        <input type="password" name="password" placeholder="Create your password" required/>
-                    </div>
-                    <div>
-                        <input type="password" name="password" placeholder="Confirm your password" required/>
-                    </div>
-                    {/* <div>
-					<input type="checkbox" name="terms" id="terms"/>
-					<label htmlFor="terms">I agree with <a href="#terms">Terms & conditions</a></label>
-				    </div> */}
-                    <button type="submit" name="signup">Sign up for free</button>
-                    <div>
-                        Already have an account? <a href="#signin">Signin here</a>
-                    </div>
-                </form>
-            </div>
-            </div>
+            <h2 className="title"> Sign up now </h2>
+          </div>
+          <div className="form ">
+            <form>
+              <div>
+                <input type="text" name="username" placeholder="Your username" required />
+              </div>
+              <div>
+                <input type="email" name="email" placeholder="Your email" required />
+              </div>
+              <div>
+                <input type="password" name="password" placeholder="Create your password" required />
+              </div>
+              <div>
+                <input type="password" name="password" placeholder="Confirm your password" required />
+              </div>
+              <button type="submit" name="signup">Sign up for free</button>
+              <div>
+                Already have an account?
+                <a href="#signin">Signin here</a>
+              </div>
+            </form>
+          </div>
         </div>
-        );
-        
-    }
+      </div>
+    );
+  }
 }
 export default Signup;
--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, } from 'react-router-dom';
 import Home from '../../components/Home';
 import Login from '../../components/Login';
+import Signup from '../../components/signup/Signup';
 import NotFound from '../../views/Error_pages/page_not_found';
 
 const Main = () => (
@@ -9,6 +10,7 @@ const Main = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
       <Route component={NotFound} />
     </Switch>
   </main>
