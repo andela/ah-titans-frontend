4639bc52add630666773a63b0d48f8023cc66d64
petermwash <petermwash48@gmail.com>
2018-08-30 16:14:22 +0300

    [Chore #160088425] Remove commented out code

--- src/Signup/containers/Signup.js
+++ src/Signup/containers/Signup.js
@@ -14,37 +14,6 @@ class Signup extends Component {
       <div>
         <SignupForm />
       </div>
-      // <div className="container flex-container">
-      //   <div className="card">
-      //     <div className="box" id="sign-up">
-      //       <div className="logo">
-      //         <img src={Logo} alt="App logo" />
-      //       </div>
-      //       <h2 className="title"> Sign up now </h2>
-      //     </div>
-      //     <div className="form">
-      //       <form>
-      //         <div>
-      //           <input type="text" name="username" placeholder="Your username" required />
-      //         </div>
-      //         <div>
-      //           <input type="email" name="email" placeholder="Your email" required />
-      //         </div>
-      //         <div>
-      //           <input type="password" name="password" placeholder="Create your password" required />
-      //         </div>
-      //         <div>
-      //           <input type="password" name="password" placeholder="Confirm your password" required />
-      //         </div>
-      //         <button type="submit" name="signup">Sign up for free</button>
-      //         <div className="clear">
-      //           Already have an account?
-      //           <Link to="/login"> Login </Link>
-      //         </div>
-      //       </form>
-      //     </div>
-      //   </div>
-      // </div>
     );
   }
 }
