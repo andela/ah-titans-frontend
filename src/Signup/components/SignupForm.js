ddde2f96095c49d32ca42c0d7ff97f3ae76ab149
Janet Wairimu <janetnim4012gmail.com>
2018-09-03 11:31:28 +0300

    [Feature #160011175] Set up user signup implementation

--- src/Signup/components/SignupForm.js
+++ src/Signup/components/SignupForm.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import { Link, } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Link, } from 'react-router-dom';
 import {
-  Button,
-  Card,
-  Row,
-  Col,
-  Input,
+  Button, Card, Row, Col, Input,
 } from 'react-materialize';
 import './Signup.scss';
 
-const SignupForm = () => (
+const SignupForm = ({ onChange, onClick, }) => (
   <div style={{ marginTop: '10%', }}>
     <Row>
       <Col s={8} offset="s2">
         <Card textClassName="black-text" title="Sign up">
           <Row>
-            <Input s={6} label="Username:" />
-            <Input s={6} type="email" label="Email:" />
-            <Input s={6} type="password" label="Password:" />
-            <Input s={6} type="password" label="Repeat password:" />
+            <Input s={6} label="Username:" onChange={onChange} />
+            <Input s={6} type="email" label="Email:" onChange={onChange} />
+            <Input s={6} type="password" label="Password:" onChange={onChange} />
+            <Input s={6} type="password" label="Repeat password:" onChange={onChange} />
             <Col s={6}>
-              <Button s={6} className="blue" waves="light">
+              <Button s={6} className="blue" waves="light" onClick={onClick}>
                 SIGNUP
               </Button>
             </Col>
@@ -36,6 +32,9 @@ const SignupForm = () => (
   </div>
 );
 
-SignupForm.propTypes = {};
+SignupForm.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default SignupForm;
