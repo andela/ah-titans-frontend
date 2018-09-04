93e8f4dfb24aa34fcda0d2648b831be578750b8f
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2018-09-04 12:47:01 +0300

    [Feature #160244936] Add PropTypes to Header component

--- src/Header/components/index.js
+++ src/Header/components/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link, } from 'react-router-dom';
 import {
   Navbar, Col,
@@ -21,5 +22,7 @@ const Header = () => (
 
   </Navbar>
 );
+
+Header.propTypes = {};
 
 export default Header;
