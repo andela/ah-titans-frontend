eb8c371e55620d5e36f8f42b9c5a2c7e9b585997
Peter <petermwash48@gmail.com>
2018-09-05 17:13:39 +0300

    #160244936 Implement designs for creating an article (#17)
    * [Chore #160244936] Setup file structure
    
    * [Feature #160244936] Clear .scss
    
    * [Feature #160244936] Add navbar
    
    * [Feature #160244936] Add PropTypes to Header component
    
    * [Chore #160244936] Update test path in dangerfile
    
    * [Chore #160244936] Add tests
    
    * [Feature #160244936] Implement the create article form
    
    * [Feature #160244936] Connect the create article form to router
    
    * [Fix #160244936] Fix typo
    
    * [Fix #160244936] Fix typo
    
    * [Fix #160244936] Refactor tests
    
    * [Fix #160244936] Remove unnecessary blank line

--- src/Main/components/index.js
+++ src/Main/components/index.js
@@ -3,6 +3,7 @@ import { Switch, Route, } from 'react-router-dom';
 import Home from '../../Home/components';
 import Login from '../../Login/components';
 import Signup from '../../Signup/containers/Signup';
+import NewArticle from '../../New_Article/components/NewArticle';
 import NotFound from '../../Error_pages/components/page_not_found';
 
 const Main = () => (
@@ -11,6 +12,7 @@ const Main = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={Signup} />
+      <Route exact path="/article" component={NewArticle} />
       <Route component={NotFound} />
     </Switch>
   </main>
