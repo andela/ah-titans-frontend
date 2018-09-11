8bebb027299cfee40b2768b6dfc5799788c9cf81
petermwash <petermwash48@gmail.com>
2018-09-11 12:00:23 +0300

    [Fearture #160011180] Add a route to header

--- src/Header/components/index.js
+++ src/Header/components/index.js
@@ -9,13 +9,14 @@ import Logo from '../../assets/logo.png';
 // between routes.
 const Header = () => (
   <Navbar style={{ backgroundColor: '#3498db', }}>
-    <Col s={8} offset="m2">
-      <Col s={8}><img height="70" width="80" src={Logo} alt="Loading ..." /></Col>
-      <Col s={4}>
+    <Col s={8} offset="s2">
+      <Col s={6}><img height="70" width="80" src={Logo} alt="Loading ..." /></Col>
+      <Col s={6}>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/login">Login</Link></li>
           <li><Link to="/signup">Signup</Link></li>
+          <li><Link to="/article">New</Link></li>
         </ul>
       </Col>
     </Col>
