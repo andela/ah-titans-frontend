5493b5ffdcbb43f595906710031f46bf5aa98cd6
John Musiu <johnmusiu@gmail.com>
2018-09-11 12:18:08 +0300

    [Feature #160318853] Fix merge conflict

--- src/reducers/viewArticles.js
+++ src/reducers/viewArticles.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
   GETTING_ARTICLES,
   VIEW_ARTICLES,
@@ -29,30 +28,3 @@ const viewArticles = (state = prevState, action) => {
 };
 
 export default viewArticles;
-=======
-import { CREATE_USER, CREATE_USER_ERROR, SIGNUP_REQUEST, } from '../actions/types';
-
-const prevState = {
-  item: {},
-  error: {},
-  isFetching: false,
-};
-const signupReducer = (state = prevState, action) => {
-  switch (action.type) {
-  case SIGNUP_REQUEST:
-    return { ...state, isFetching: true, };
-  case CREATE_USER:
-    return {
-      ...state,
-      isFetching: false,
-      item: action.payload.user,
-    };
-  case CREATE_USER_ERROR:
-    return { ...state, error: action.payload.errors, isFetching: false, };
-  default:
-    return state;
-  }
-};
-
-export default signupReducer;
->>>>>>> d934da74b5afea2496367d7c50aeb352393013e9
