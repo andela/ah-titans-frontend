7c7cdc95d91c90abf26288e11c362dc0d5f1d0e7
Janet Wairimu <janetnim4012gmail.com>
2018-09-11 11:17:53 +0300

    [Chore #160318853] Fix weak password validation

--- src/Home/components/index.js
+++ src/Home/components/index.js
@@ -20,10 +20,7 @@ const ArticlesForm = () => (
       <Row>
         <Col s={12} m={10}>
           <div className="search-bar">
-            <Button>Search By</Button>
-            {/* </Col> */}
-
-            {/* <Col s={12} m={8}> */}
+            <Button>Filter By</Button>
             <div className="input-field">
               <input id="search" type="search" required />
               <label htmlFor="search">
@@ -34,8 +31,59 @@ const ArticlesForm = () => (
           </div>
         </Col>
       </Row>
-
-      <Col m={6} s={12} className="articles">
+      <Col s={12} m={6}>
+        <Card
+          className="grey lighten-4"
+          textClassName="black-text"
+          title="How to train your dragon"
+          actions={[
+            <button className="btn waves-effect waves-light blue-grey">
+              <i className="material-icons">share</i>
+            </button>,
+            <button className="btn waves-effect waves-light blue-grey">
+              <i className="material-icons">thumb_up</i>
+              <span>77</span>
+            </button>,
+            <button className="btn waves-effect waves-light blue-grey">
+              <i className="material-icons">thumb_down</i>
+              <span>19</span>
+            </button>,
+            <a
+              className="right blue-grey-text"
+              href="http://www.tutorialspoint.com"
+            >
+              Read more...
+            </a>,
+          ]}
+        >
+          <Chip>dragon training</Chip>
+          <p className="ratings">
+            <i className="material-icons icon-orange">star</i>
+            <i className="material-icons icon-orange">star</i>
+            <i className="material-icons icon-orange">star</i>
+            <i className="material-icons icon-orange">star</i>
+            <i className="material-icons icon-orange">star_half</i>
+            <div className="stat">4.6</div>
+          </p>
+          <p>
+            <i className="about">Wanna know how not to get devoured?</i>
+          </p>
+          <br />
+          <p>
+            {' '}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud...
+          </p>
+          <br />
+          <div className="writtenby">
+            By: James Pink{' '}
+            <a href="url" className="follow">
+              Follow
+            </a>
+            01/08/2018 08:12am
+          </div>
+        </Card>
         <Card
           className="grey lighten-4"
           textClassName="black-text"
