87a79fe3a243ee66190f2b52f14473954cb73f27
Janet Wairimu <janetnim4012gmail.com>
2018-09-11 13:39:22 +0300

    [Feature #160318853] Add filter by dropdown icon

--- src/Home/components/index.js
+++ src/Home/components/index.js
@@ -13,7 +13,14 @@ const ArticlesForm = () => (
       <Row>
         <Col s={12} m={10}>
           <div className="search-bar">
-            <Dropdown trigger={<Button>Filter By</Button>}>
+            <Dropdown
+              trigger={(
+                <Button>
+                  Filter By
+                  <i className="material-icons">filter_list</i>
+                </Button>
+              )}
+            >
               <NavItem>Tags</NavItem>
               <NavItem>Author</NavItem>
               <NavItem>Title</NavItem>
@@ -29,55 +36,6 @@ const ArticlesForm = () => (
         </Col>
       </Row>
       <Col s={12} m={6}>
-        <Card
-          className="grey lighten-4"
-          textClassName="black-text"
-          title="How to train your dragon"
-          actions={[
-            <button className="btn waves-effect waves-light blue-grey">
-              <i className="material-icons">share</i>
-            </button>,
-            <button className="btn waves-effect waves-light blue-grey">
-              <i className="material-icons">thumb_up</i>
-              <span>77</span>
-            </button>,
-            <button className="btn waves-effect waves-light blue-grey">
-              <i className="material-icons">thumb_down</i>
-              <span>19</span>
-            </button>,
-            <a className="right blue-grey-text" href="http://www.tutorialspoint.com">
-              Read more...
-            </a>,
-          ]}
-        >
-          <Chip>dragon training</Chip>
-          <p className="ratings">
-            <i className="material-icons icon-orange">star</i>
-            <i className="material-icons icon-orange">star</i>
-            <i className="material-icons icon-orange">star</i>
-            <i className="material-icons icon-orange">star</i>
-            <i className="material-icons icon-orange">star_half</i>
-            4.6
-          </p>
-          <p>
-            <i className="about">Wanna know how not to get devoured?</i>
-          </p>
-          <br />
-          <p>
-            {' '}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud...
-          </p>
-          <br />
-          <div className="writtenby">
-            By: James Pink
-            {' '}
-            <a href="url" className="follow">
-              Follow
-            </a>
-            01/08/2018 08:12am
-          </div>
-        </Card>
         <Card
           className="grey lighten-4"
           textClassName="black-text"
