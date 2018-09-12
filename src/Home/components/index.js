3a839a7bfb5686b103e83a4c0ef4c5e26b04c5a5
John Musiu <johnmusiu@gmail.com>
2018-09-12 14:47:52 +0300

    [Feature #160318853] merge conflicts

--- src/Home/components/index.js
+++ src/Home/components/index.js
@@ -1,72 +1,8 @@
 import React from 'react';
 import moment from 'moment';
-import {
-  Card, Col, Row, Chip, Icon,
-} from 'react-materialize';
+import { Card, Col, Chip, Icon } from 'react-materialize';
 import './index.scss';
 
-<<<<<<< HEAD
-const ArticlesForm = ({ article, }) => (
-  <Row className="home">
-    <Col s={12} m={5}>
-      <Card
-        key={article.slug}
-        className="grey lighten-4"
-        textClassName="black-text"
-        title={article.title}
-        actions={[
-          <button className="btn waves-effect waves-light blue-grey">
-            <i className="material-icons">share</i>
-          </button>,
-          <button className="btn waves-effect waves-light blue-grey">
-            <i className="material-icons">thumb_up</i>
-            <span>{article.likes_count}</span>
-          </button>,
-          <button className="btn waves-effect waves-light blue-grey">
-            <i className="material-icons">thumb_down</i>
-            <span>{article.dislikes_count}</span>
-          </button>,
-          <a className="right blue-grey-text" href="http://www.tutorialspoint.com">
-            Read more...
-          </a>,
-        ]}
-      >
-        {article.tagList.map(tag => <Chip>{tag}</Chip>)}
-
-        <p className="ratings">
-          <Icon blue>star</Icon>
-          <i className="material-icons">star</i>
-          <i className="material-icons icon-orange">star</i>
-          <i className="material-icons icon-orange">star</i>
-          <i className="material-icons icon-orange">star_half</i>
-          <i style={{ alignContent: 'center', position: 'inline', }}>(4.6)</i>
-          <i className="material-icons icon-blue right">
-            {article.favorited ? 'favorite' : 'favorite_border'}
-          </i>
-        </p>
-
-        <p />
-        <p className="about">{article.description}</p>
-        <br />
-        <p>
-          {' '}
-          {article.body}
-        </p>
-        <br />
-        <div className="writtenby left">
-          By:
-          {' '}
-          {article.author.username}
-          {' '}
-          <a href="url" className="follow">
-            Follow
-          </a>
-          {moment(article.created_at).format('MMM Do YYYY, h:mm:ss a')}
-        </div>
-      </Card>
-    </Col>
-  </Row>
-=======
 const ArticlesForm = ({ article }) => (
   <Col s={12} m={6}>
     <Card
@@ -124,7 +60,6 @@ const ArticlesForm = ({ article }) => (
       </p>
     </Card>
   </Col>
->>>>>>> d38e971fd77cf115a1e9549f38c9e8795d44e6e0
 );
 
 ArticlesForm.propTypes = {};
