9779c1df878eec3ce54bd14fe5d2cf22bc8db59e
John Musiu <johnmusiu@gmail.com>
2018-09-12 14:41:51 +0300

    [Feature #160318853] Update page design to match prototype

--- src/Home/components/index.js
+++ src/Home/components/index.js
@@ -4,64 +4,62 @@ import { Card, Col, Row, Chip, Icon } from 'react-materialize';
 import './index.scss';
 
 const ArticlesForm = ({ article }) => (
-  <Row className="home">
-    <Col s={12} m={5}>
-      <Card
-        key={article.slug}
-        className="grey lighten-4"
-        textClassName="black-text"
-        title={article.title}
-        actions={[
-          <button className="btn waves-effect waves-light blue-grey">
-            <i className="material-icons">share</i>
-          </button>,
-          <button className="btn waves-effect waves-light blue-grey">
-            <i className="material-icons">thumb_up</i>
-            <span>{article.likes_count}</span>
-          </button>,
-          <button className="btn waves-effect waves-light blue-grey">
-            <i className="material-icons">thumb_down</i>
-            <span>{article.dislikes_count}</span>
-          </button>,
-          <a
-            className="right blue-grey-text"
-            href="http://www.tutorialspoint.com"
-          >
-            Read more...
-          </a>,
-        ]}
-      >
-        {article.tagList.map(tag => {
-          return <Chip>{tag}</Chip>;
-        })}
+  <Col s={12} m={6}>
+    <Card
+      key={article.slug}
+      className="grey lighten-4 article"
+      textClassName="black-text"
+      title={article.title}
+      actions={[
+        <button className="btn waves-effect waves-light blue-grey">
+          <i className="material-icons">share</i>
+        </button>,
+        <button className="btn waves-effect waves-light blue-grey">
+          <i className="material-icons">thumb_up</i>
+          <span>{article.likes_count}</span>
+        </button>,
+        <button className="btn waves-effect waves-light blue-grey">
+          <i className="material-icons">thumb_down</i>
+          <span>{article.dislikes_count}</span>
+        </button>,
+        <a className="right blue-grey-text" href="article link goes here">
+          Read more...
+        </a>,
+      ]}
+    >
+      {article.tagList.map(tag => {
+        return <Chip>{tag}</Chip>;
+      })}
 
-        <p className="ratings">
-          <Icon blue>star</Icon>
-          <i className="material-icons">star</i>
-          <i className="material-icons icon-orange">star</i>
-          <i className="material-icons icon-orange">star</i>
-          <i className="material-icons icon-orange">star_half</i>
-          <i style={{ alignContent: 'center', position: 'inline' }}>(4.6)</i>
-          <i className="material-icons icon-blue right">
-            {article.favorited ? 'favorite' : 'favorite_border'}
-          </i>
-        </p>
+      <p className="ratings">
+        <Icon blue>star</Icon>
+        <i className="material-icons">star</i>
+        <i className="material-icons icon-orange">star</i>
+        <i className="material-icons icon-orange">star</i>
+        <i className="material-icons icon-orange">star_half</i>
+        <i style={{ alignContent: 'center', position: 'inline' }}>(4.6)</i>
+        <i className="material-icons icon-blue right">
+          {article.favorited ? 'favorite' : 'favorite_border'}
+        </i>
+      </p>
 
-        <p />
-        <p className="about">{article.description}</p>
-        <br />
-        <p> {article.body}</p>
-        <br />
-        <div className="writtenby left">
-          By: {article.author.username}{' '}
-          <a href="url" className="follow">
-            Follow
-          </a>
-          {moment(article.created_at).format('MMM Do YYYY, h:mm:ss a')}
-        </div>
-      </Card>
-    </Col>
-  </Row>
+      <p />
+      <p className="about">{article.description}</p>
+      <br />
+      <p>
+        {article.body.substring(0, 150)}
+        {article.body.length > 150 ? '...' : ''}
+      </p>
+      <hr className="light-grey" />
+      <p className="writtenby">
+        By: {article.author.username}
+        <a href="url" className="follow">
+          Follow
+        </a>
+        {moment(article.created_at).format('MMM Do YYYY, h:mm:ss a')}
+      </p>
+    </Card>
+  </Col>
 );
 
 ArticlesForm.propTypes = {};
