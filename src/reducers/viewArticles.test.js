6de01377bc5e5ef99ce242f06594c42c5339615c
Janet Wairimu <janetnim4012gmail.com>
2018-09-12 12:52:33 +0300

    [Feature #160318853] Write tests for viewArticles reducer

---
+++ src/reducers/viewArticles.test.js
@@ -0,0 +1,50 @@
+import * as types from '../actions/types';
+import reducer from './viewArticles';
+
+describe('viewArticles reducer', () => {
+  const initialState = {
+    items: {},
+    error: {},
+    isFetching: false,
+    success: false,
+  };
+
+  it('should return the initial state', () => {
+    expect(reducer(initialState, {})).toEqual(initialState);
+  });
+
+  it('should set isFetching state to true', () => {
+    const startAction = {
+      type: types.GETTING_ARTICLES,
+    };
+
+    expect(reducer(initialState, startAction)).toEqual({
+      ...initialState,
+      isFetching: true,
+      success: false,
+    });
+  });
+
+  it('should return an error', () => {
+    const startAction = {
+      type: types.CREATE_USER_ERROR,
+      payload: { errors: {}, },
+    };
+
+    expect(reducer(initialState, startAction)).toEqual({
+      ...initialState,
+      error: {},
+    });
+  });
+
+  it('should return an article object', () => {
+    const startAction = {
+      type: types.VIEW_ARTICLES,
+      payload: { article: { items, }, },
+    };
+
+    expect(reducer(initialState, startAction)).toEqual({
+      ...initialState,
+    });
+  });
+});
