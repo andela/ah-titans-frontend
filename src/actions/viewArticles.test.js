f6f3617af3502618b50016bf01b4da10a7a300ab
Janet Wairimu <janetnim4012gmail.com>
2018-09-12 16:28:44 +0300

    [Feature #160318853] Add tests

--- src/actions/viewArticles.test.js
+++ src/actions/viewArticles.test.js
@@ -1,3 +1,4 @@
+import configureStore from 'redux-mock-store';
 import getArticle, {
   getArticlesErrorActionCreator,
   gettingArticlesActionCreator,
@@ -5,6 +6,9 @@ import getArticle, {
 } from './viewArticles';
 
 import { VIEW_ARTICLES, GETTING_ARTICLES, VIEW_ARTICLES_ERROR, } from './types';
+
+const mockStore = configureStore();
+const store = mockStore();
 
 const expectedAction = type => ({
   type,
@@ -36,5 +40,55 @@ describe('fetch articles', () => {
     },
   }).then((res) => {
     expect(res.body).toEqual(JSON.parse());
+  });
+});
+
+describe('select view articles action', () => {
+  const articles = {};
+
+  test('Dispatches view articles action and payload', () => {
+    const expectedActions = [
+      {
+        type: 'VIEW_ARTICLES',
+        payload: articles,
+      },
+    ];
+
+    store.dispatch(getArticlesActionCreator(articles));
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+});
+
+describe('select view articles errors action', () => {
+  const errors = {};
+  beforeEach(() => {
+    store.clearActions();
+  });
+  test('Dispatches view articles errors action and payload', () => {
+    const expectedActions = [
+      {
+        type: 'VIEW_ARTICLES_ERROR',
+        payload: errors,
+      },
+    ];
+
+    store.dispatch(getArticlesErrorActionCreator(errors));
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+});
+
+describe('select getting articles action', () => {
+  beforeEach(() => {
+    store.clearActions();
+  });
+  test('Dispatches getting articles action and payload', () => {
+    const expectedActions2 = [
+      {
+        type: 'GETTING_ARTICLES',
+      },
+    ];
+
+    store.dispatch(gettingArticlesActionCreator());
+    expect(store.getActions()).toEqual(expectedActions2);
   });
 });
