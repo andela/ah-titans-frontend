34fd0867eb7b43b7c1bf496432fd9a558722f000
Janet Wairimu <janetnim4012gmail.com>
2018-09-12 19:07:56 +0300

    [Feature #160318853] Add tests

--- src/Home/homepage.test.js
+++ src/Home/homepage.test.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { shallow, } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import Home from './containers/index';
 import store from '../store';
 import ArticlesForm from './components/index';
+import SearchComponent from './components/search';
 
 describe('Home', () => {
   it('renders without crashing', () => {
     shallow(<Home store={store} />);
+  });
+});
+
+describe('<SearchComponent />', () => {
+  describe('render()', () => {
+    test('renders the component', () => {
+      const wrapper = shallow(<SearchComponent />);
+      const component = wrapper.dive();
+
+      expect(toJson(component)).toMatchSnapshot();
+    });
+  });
+});
+
+describe('<ArticlesForm />', () => {
+  describe('render()', () => {
+    const article = { article: { slug: '', tagList: ['any', ], body: 'this is a test body', }, };
+    test('renders the component', () => {
+      const wrapper = shallow(<ArticlesForm article={article} />);
+      const component = wrapper.dive();
+
+      expect(toJson(component)).toMatchSnapshot();
+    });
   });
 });
 
