4a336f770f89aa6dbe61448299c9dc74a7aac164
chepmanu <emmanuelchepsire33@gmail.com>
2018-09-13 17:40:22 +0300

    [Chore #160011178] Implemented social login

--- src/Login/containers/index.js
+++ src/Login/containers/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Alert from 'react-s-alert';
 import loginUser from '../../actions/loginActions';
 import LoginForm from '../components';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/slide.css';
+import googleLogin from '../../actions/googleActions';
+import facebookLogin from '../../actions/facebookActions';
 
 class Login extends React.Component {
 	constructor(props) {
@@ -14,9 +19,16 @@ class Login extends React.Component {
 		};
 		const { history } = this.props;
 		this.history = history;
-
+		this.googleResponse = this.googleResponse.bind(this);
+		this.facebookResponse = this.facebookResponse.bind(this);
+		this.onFailure = this.onFailure.bind(this);
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	onFailure() {
+		Alert.error('An error occurred with the social media login, try login by email');
+		this.history.push('/login');
 	}
 
 	handleChange(e) {
@@ -25,7 +37,6 @@ class Login extends React.Component {
 
 	handleSubmit(e) {
 		e.preventDefault();
-
 		const { email, password } = this.state;
 
 		const user = {
@@ -38,15 +49,36 @@ class Login extends React.Component {
 		this.props.loginUser(user, this.props.history);
 	}
 
+	googleResponse(response) {
+		const requestBody = {
+			access_token: response.accessToken,
+		};
+		this.props.googleLogin(requestBody, this.props.history);
+	}
+
+	facebookResponse(response) {
+		const requestBody = {
+			access_token: response.accessToken,
+		};
+		this.props.facebookLogin(requestBody, this.props.history);
+	}
+
 	render() {
 		const { login } = this.props;
-		const { errors, isFetching } = login;
+		const {
+			errors, isFetching, isFetchingFacebook, isFetchingGoogle,
+		} = login;
 		return (
 			<LoginForm
 				onChange={this.handleChange}
 				onClick={this.handleSubmit}
 				errors={errors}
 				isFetching={isFetching}
+				isFetchingFacebook={isFetchingFacebook}
+				isFetchingGoogle={isFetchingGoogle}
+				onFailure={this.onFailure}
+				onSuccess={this.googleResponse}
+				facebookResponse={this.facebookResponse}
 			/>
 		);
 	}
@@ -54,13 +86,14 @@ class Login extends React.Component {
 
 Login.propTypes = {
 	loginUser: PropTypes.func.isRequired,
+	googleLogin: PropTypes.func.isRequired,
+	facebookLogin: PropTypes.func.isRequired,
 	login: PropTypes.shape({
 		user: PropTypes.object,
 		errors: PropTypes.object,
 		isFetching: PropTypes.bool,
-	}).isRequired,
-	history: PropTypes.shape({
-		push: PropTypes.func.isRequired,
+		isFetchingGoogle: PropTypes.bool,
+		isFetchingFacebook: PropTypes.bool,
 	}).isRequired,
 };
 
@@ -68,11 +101,16 @@ const mapStateToProps = ({ login }) => ({
 	login,
 });
 
-
 const mapDispatchToProps = (dispatch) => {
 	return {
 		loginUser: (userData, history) => {
 			dispatch(loginUser(userData, history));
+		},
+		googleLogin: (requestBody, history) => {
+			dispatch(googleLogin(requestBody, history));
+		},
+		facebookLogin: (requestBody, history) => {
+			dispatch(facebookLogin(requestBody, history));
 		},
 	};
 };
