5fb15b1fd5870d5ddbd4803adfb1d132b3eb650f
Janet Wairimu <janetnim4012gmail.com>
2018-09-13 09:34:52 +0300

    [Feature #160318853] Remove commented code

--- src/Loader/loader.test.js
+++ src/Loader/loader.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { shallow, } from 'enzyme';
-// import toJson from 'enzyme-to-json';
 import Loader from './components/index';
 import store from '../store';
-// import ArticlesForm from './components/index';
-// import SearchComponent from './components/search';
 
 describe('Loader', () => {
   it('renders without crashing', () => {
