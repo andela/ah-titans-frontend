d0461ff8ccc44f444fc10f477a196862d177b0ad
Janet Wairimu <janetnim4012gmail.com>
2018-09-13 09:34:38 +0300

    [Feature #160318853] Remove commented code

--- src/Loader/loader.test.js
+++ src/Loader/loader.test.js
@@ -11,34 +11,3 @@ describe('Loader', () => {
     shallow(<Loader store={store} />);
   });
 });
-
-// describe('<SearchComponent />', () => {
-//   describe('render()', () => {
-//     test('renders the component', () => {
-//       const wrapper = shallow(<SearchComponent />);
-//       const component = wrapper.dive();
-
-//       expect(toJson(component)).toMatchSnapshot();
-//     });
-//   });
-// });
-
-// describe('<ArticlesForm />', () => {
-//   describe('render()', () => {
-//     const article = { article: { slug: '', tagList: ['any', ], body: 'this is a test body', }, };
-//     test('renders the component', () => {
-//       const wrapper = shallow(<ArticlesForm article={article} />);
-//       const component = wrapper.dive();
-
-//       expect(toJson(component)).toMatchSnapshot();
-//     });
-//   });
-// });
-
-// // describe('Articles form', () => {
-// //   const articlesFormWrapper = shallow(<ArticlesForm store={store} error={{}} success key={{}} />);
-
-//   it('renders error fields ', () => {
-//     expect(articlesFormWrapper.find('.error').length).toEqual(1);
-//   });
-// });
