b51eba33176853ca4876320af48c7edabeb0b4d1
Janet Wairimu <janetnim4012gmail.com>
2018-09-13 17:48:59 +0300

    [Feature #160318853] Call articles fetch api

--- src/actions/viewArticles.js
+++ src/actions/viewArticles.js
@@ -1,36 +1,48 @@
-import { VIEW_ARTICLES, VIEW_ARTICLES_ERROR, GETTING_ARTICLES, } from './types';
+import { VIEW_ARTICLES, VIEW_ARTICLES_ERROR, GETTING_ARTICLES } from './types';
+import call from '../utils/service';
 
 export const gettingArticlesActionCreator = () => ({
-  type: GETTING_ARTICLES,
+	type: GETTING_ARTICLES,
 });
 
 export const getArticlesActionCreator = articles => ({
-  type: VIEW_ARTICLES,
-  payload: articles,
+	type: VIEW_ARTICLES,
+	payload: articles,
 });
 
 export const getArticlesErrorActionCreator = error => ({
-  type: VIEW_ARTICLES_ERROR,
-  payload: error,
-});
-
-const fetchArticles = () => fetch('https://ah-titans-api.herokuapp.com/api/articles/').then(handleResponse);
-
-const handleResponse = response => response.text().then((text) => {
-  const data = text && JSON.parse(text);
-  if (!response.ok) {
-    return Promise.reject(data);
-  }
-  return data;
+	type: VIEW_ARTICLES_ERROR,
+	payload: error,
 });
 
 const getArticles = () => (dispatch) => {
-  dispatch(gettingArticlesActionCreator());
-  fetchArticles()
-    .then((articles) => {
-      dispatch(getArticlesActionCreator(articles));
-    })
-    .catch(error => dispatch(getArticlesErrorActionCreator(error)));
+	dispatch(
+		gettingArticlesActionCreator(),
+	);
+	call(
+		{
+			endpoint:
+				'/articles/',
+			method:
+				'GET',
+		},
+	)
+		.then(
+			(articles) => {
+				dispatch(
+					getArticlesActionCreator(
+						articles,
+					),
+				);
+			},
+		)
+		.catch(
+			error => dispatch(
+				getArticlesErrorActionCreator(
+					error,
+				),
+			),
+		);
 };
 
 export default getArticles;
