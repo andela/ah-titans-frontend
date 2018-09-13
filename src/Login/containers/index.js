69825aef6f1cfb42759b67b3e902537b5cba0c02
chepmanu <emmanuelchepsire33@gmail.com>
2018-09-13 17:40:22 +0300

    [Chore #160011178] Implemented social login

--- src/Login/containers/index.js
+++ src/Login/containers/index.js
@@ -1,66 +1,104 @@
 import React from 'react';
-import { connect, } from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import loginUser from '../../actions/loginActions';
 import LoginForm from '../components';
+import call from '../../utils/service';
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      email: '',
-      password: '',
-    };
+		this.state = {
+			email: '',
+			password: '',
+		};
+		const { history } = this.props;
+		this.history = history;
+		this.googleResponse = this.googleResponse.bind(this);
+		this.facebookResponse = this.facebookResponse.bind(this);
+		this.onFailure = this.onFailure.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+	onFailure(error) {
+		error.toString();
+		this.history.push('/login');
+		// console.log(error, 'This is bad');
+	}
 
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value, });
-  }
+	handleChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	}
 
-  handleSubmit(e) {
-    e.preventDefault();
+	handleSubmit(e) {
+		e.preventDefault();
+		const { email, password } = this.state;
 
-    const { email, password, } = this.state;
+		const user = {
+			user: {
+				email,
+				password,
+			},
+		};
 
-    const user = {
-      user: {
-        email,
-        password,
-      },
-    };
+		this.props.loginUser(user, this.props.history);
+	}
 
-    this.props.loginUser(user, this.props.history);
-  }
+	googleResponse(response) {
+		const requestBody = {
+			access_token: response.accessToken,
+		};
+		call({ endpoint: '/users/auth/google-oauth2', method: 'POST', data: requestBody })
+			.then(((res) => {
+				localStorage.setItem('token', res.user.token);
+				localStorage.setItem('user', res.user.user.username);
+			}));
+		this.history.push('/');
+	}
 
-  render() {
-    const { login, } = this.props;
-    const { errors, isFetching, } = login;
-    return (
-      <LoginForm
-        onChange={this.handleChange}
-        onClick={this.handleSubmit}
-        errors={errors}
-        isFetching={isFetching}
+	facebookResponse(response) {
+		const requestBody = {
+			access_token: response.accessToken,
+		};
+		call({ endpoint: '/users/auth/facebook', method: 'POST', data: requestBody })
+			.then(((res) => {
+				localStorage.setItem('token', res.user.token);
+				localStorage.setItem('user', res.user.user.username);
+			}));
+		this.history.push('/');
+	}
+
+
+	render() {
+		const { login } = this.props;
+		const { errors, isFetching } = login;
+		return (
+  <LoginForm
+				onChange={this.handleChange}
+				onClick={this.handleSubmit}
+				errors={errors}
+				isFetching={isFetching}
+				onFailure={this.onFailure}
+				onSuccess={this.googleResponse}
+				facebookResponse={this.facebookResponse}
       />
-    );
-  }
+		);
+	}
 }
 
 Login.propTypes = {
-  loginUser: PropTypes.func.isRequired,
-  login: PropTypes.shape({
-    user: PropTypes.object,
-    errors: PropTypes.object,
-    isFetching: PropTypes.bool,
-  }).isRequired,
+	loginUser: PropTypes.func.isRequired,
+	login: PropTypes.shape({
+		user: PropTypes.object,
+		errors: PropTypes.object,
+		isFetching: PropTypes.bool,
+	}).isRequired,
 };
 
-const mapStateToProps = ({ login, }) => ({
-  login,
+const mapStateToProps = ({ login }) => ({
+	login,
 });
 
-export default connect(mapStateToProps, { loginUser, })(Login);
+export default connect(mapStateToProps, { loginUser })(Login);
