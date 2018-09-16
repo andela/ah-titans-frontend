eedc5e2f9e4b87988244bcf424c9e174cf1b9b88
chepmanu <emmanuelchepsire33@gmail.com>
2018-09-17 00:59:50 +0300

    [Chore #160011178] Change the button designs

--- src/actions/signup.js
+++ src/actions/signup.js
@@ -4,24 +4,33 @@ import call from '../utils/service';
 export const signingUp = () => ({ type: SIGNUP_REQUEST });
 
 export const createUserActionCreator = user => ({
-  type: CREATE_USER,
-  payload: user,
+	type: CREATE_USER,
+	payload: user,
 });
 
 export const createUserErrorActionCreator = error => ({
-  type: CREATE_USER_ERROR,
-  payload: error,
+	type: CREATE_USER_ERROR,
+	payload: error,
 });
 
+/**
+ * Represents functionality for creating a user.
+ * @constructor
+ * @param {function} history - Handles routing to the next page.
+ * * @param {object} user - Contains the typed in user information.
+ * @access - Public for both registered and unregistered users.
+ */
+
+
 const createUser = (userData, history) => dispatch => {
-  dispatch(signingUp());
-  call({ endpoint: '/users/', method: 'POST', data: userData })
-    .then(data => {
-      dispatch(createUserActionCreator(data));
-      localStorage.setItem('user', data);
-      history.push('/');
-    })
-    .catch(error => dispatch(createUserErrorActionCreator(error)));
-};
+	dispatch(signingUp());
+	call({ endpoint: '/users/', method: 'POST', data: userData })
+	  .then(data => {
+		dispatch(createUserActionCreator(data));
+		localStorage.setItem('user', data);
+		history.push('/');
+	  })
+	  .catch(error => dispatch(createUserErrorActionCreator(error)));
+	};
 
 export default createUser;
