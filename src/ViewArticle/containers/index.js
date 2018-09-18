bc042f92b5ba3607b9d12e716683855277cd1c92
petermwash <petermwash48@gmail.com>
2018-09-18 10:09:51 +0300

    [Feature #160415392]  Add Edit button

--- src/ViewArticle/containers/index.js
+++ src/ViewArticle/containers/index.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import ViewArticle from '../components';
+import EditButton from '../components/EditButton';
 
 class ArticleView extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: 'Title',
+			description: 'The Description',
+			body: 'Blahblahblah Blahblahblah Blahblahblah Blahblahblah Blahblahblah Blahblahblah',
+		};
+	}
+
+	handleEdit() {
+		console.log('Editing...');
+		this.setState({
+			description: 'Article editted',
+		});
+	}
+
 	render() {
+		const { title, description, body } = this.state;
 		return (
-			<ViewArticle />
+			<div>
+				<ViewArticle
+					title={title}
+					description={description}
+					body={body}
+				/>
+				<EditButton
+					handleEdit={this.handleEdit}
+				/>
+			</div>
 		);
 	}
 }
