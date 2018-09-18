f1703113c902928220bf4054c7f242695166e9f6
petermwash <petermwash48@gmail.com>
2018-09-18 19:40:01 +0300

    [Chore #160415392]  Fix conflicts

--- src/Signup/Signup.test.js
+++ src/Signup/Signup.test.js
@@ -1,11 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
 import { MemoryRouter, } from 'react-router-dom';
 import { Provider, } from 'react-redux';
 import { shallow, mount } from 'enzyme';
-=======
-import { shallow} from 'enzyme';
->>>>>>> dd958961cf1a6579d9d74da3f500a7234e6ecac6
 import SignupForm from './components/SignupForm';
 import Signup from './containers/Signup';
 import store from '../store';
