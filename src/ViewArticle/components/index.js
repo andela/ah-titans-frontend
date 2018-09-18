02286446beb123fe98fd409ab6803bdf8f1b9eee
Janet Wairimu <janetnim4012gmail.com>
2018-09-18 10:09:42 +0300

    [Feature #160415392] Add article icons

--- src/ViewArticle/components/index.js
+++ src/ViewArticle/components/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col } from 'react-materialize';
+import { Col , Icon, Row } from 'react-materialize';
 import moment from 'moment';
 
 class ViewArticle extends React.Component {
@@ -7,25 +7,49 @@ class ViewArticle extends React.Component {
 		const {article} = this.props;
 
 		return (
-			<Col s={12} m={12}>
+			<Row s={12}>
+				<Col s={12}>
 				<div className="container">
-					<div>
-						<h2>{article.title}</h2>
-					</div>
-                    <div>
+					<Row s={12}>
+						<Col s={6}>
+						<div>
+						<h3 style={{color : 'blue'}}>{article.title}</h3>
+						</div>
+					</Col>
+					<Col s={6}>
+						<div>
+							<i className="material-icons icon-orange small">star</i>
+							<i className="material-icons icon-orange small">star</i>
+							<i className="material-icons icon-orange small">star</i>
+							<i className="material-icons icon-orange small">star</i>
+							<i className="material-icons icon-orange small">star_half</i>
+							<span style={{ alignContent: 'center', position: 'inline', }}>4.6</span>
+						</div>
+					</Col>
+					</Row>
+					<Col>
+							<i className="material-icons icon-blue right">
+								{article.favorited ? 'favorite' : 'favorite_border'}
+							</i>
+					</Col>
+                   <Col s={12}>
+				   	 <div>
                         <h6><i><b>By: {article.author && article.author.username}</b></i></h6>
                     </div>
-                    <div>
+				   </Col>
+                    <Col s={12}>
+						<div>
                         <h6><i><b>{moment(article.updated_at).format('MMM Do YYYY, h:mm:ss a')}</b></i></h6>
-                    </div>
-					<div>
-						<h4>{article.description}</h4>
-					</div>
+                    	</div>
+					</Col>
+					<Col s={12}>
 					<div>
 						<p>{article.body}</p>
 					</div>
+					</Col>
 				</div>
 			</Col>
+			</Row>
 		);
 	}
 }
