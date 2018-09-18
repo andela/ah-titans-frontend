adcd7bce3b761e39ae330860c2289616a953fb93
Janet Nim <janetnim401@gmail.com>
2018-09-18 14:59:27 +0300

    Edit indentation

--- src/ViewArticle/components/EditButton.js
+++ src/ViewArticle/components/EditButton.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Button } from 'react-materialize';
 
 class EditButton extends React.Component {
-	render() {
-		const { handleEdit } = this.props;
-		return (
-			<div>
-				<Button onClick={handleEdit} floating large className="green" fab="vertical" tooltip="Edit article" waves="light" icon="edit" />
-			</div>
-		);
-	}
+     render() {
+       const { handleEdit } = this.props;
+	  return (
+	    <div>
+	      <Button onClick={handleEdit} floating large className="green" fab="vertical" tooltip="Edit article" waves="light" icon="edit" />
+	    </div>
+	  );
+       }
 }
 
 export default EditButton;
