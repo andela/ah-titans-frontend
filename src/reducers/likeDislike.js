9c3b361f30fb3f6c5062ac337c0fedb9430105d0
Janet Wairimu <janetnim4012gmail.com>
2018-09-19 09:56:01 +0300

    [Feature #160011184] Create like dislike reducer

---
+++ src/reducers/likeDislike.js
@@ -0,0 +1,36 @@
+import { LIKE_ARTICLE, DISLIKE_ARTICLE, LIKE_DISLIKE_ERROR } from '../actions/types';
+
+const prevState = {
+  error: {},
+  isLiking: false,
+  isDisliking: false,
+};
+
+const likeDislikeArticles = (state = prevState, action) => {
+  switch (
+  action.type
+  ) {
+    case LIKE_ARTICLE:
+      return {
+        ...state,
+        isLiking: true,
+      };
+    case DISLIKE_ARTICLE:
+      return {
+        ...state,
+        isDisliking: true,
+      };
+    case LIKE_DISLIKE_ERROR:
+      return {
+        ...state,
+        error:
+          action
+            .payload
+            .errors,
+      };
+    default:
+      return state;
+  }
+};
+
+export default likeDislikeArticles;
