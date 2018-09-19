c637bb2b6dd0136f30fc4a5c036fe7a0eead4f67
John Musiu <johnmusiu@gmail.com>
2018-09-19 11:22:53 +0300

    [Chore #160538683] Fix

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store';
 
+if (process.env.NODE_ENV !== 'production') {
+	console.log('Looks like we are in development mode!');
+}
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root'),
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root'),
 );
