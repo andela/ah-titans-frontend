4df056f985ddd070e1d41c3354e7cb3e38c1a1ed
John Musiu <johnmusiu@gmail.com>
2018-09-19 10:05:59 +0300

    [Chore #160538683] Implement get current user  into state

--- src/Home/containers/index.js
+++ src/Home/containers/index.js
@@ -1,7 +1,8 @@
-import React, { Component, } from 'react';
+import React, { Component } from 'react';
 import SnackBar from 'react-material-snackbar';
-import { connect, } from 'react-redux';
-import { Row, } from 'react-materialize';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Row } from 'react-materialize';
 import '../components/index.scss';
 import ArticlesForm from '../components';
 import SearchComponent from '../components/search';
@@ -12,64 +13,46 @@ import { bindActionCreators } from 'redux';
 import getActiveUser from '../../actions/getActiveUser';
 
 export class Home extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      isFetching: '',
-      success: '',
-      articles: {}
-    }
-  }
+	componentDidMount() {
+		this.props.getArticles();
+		this.props.getActiveUser();
+	}
 
-  componentDidMount() {
-    this.props.getArticles();
-    console.log(getActiveUser());
-  }
+	render() {
+		const { new_user } = this.props.home;
+		const { isFetching, success } = this.props.articles;
+		const articles = this.props.articles.items.results;
+		const article = [];
+		if (success === true) {
+			articles.map(article_ => article.push(<ArticlesForm article={article_} key={article.slug} />),);
+		}
 
-  componentWillReceiveProps(nextProps){
-    const {articles} = nextProps
-    const {isFetching, success} = articles
-    const all_articles  = articles.items.results
-    this.setState({isFetching, success, articles:all_articles})
-  }
-
-  render() {
-    const { new_user, } = this.props.home;
-    const { isFetching, success, articles} = this.state;
-    const article = [];
-    const token = localStorage.getItem('token');
-
-    if (success === true) {
-      articles.map(article_ => article.push(<ArticlesForm article={article_} key={article.slug} />));
-    }
-
-    return (
-      <div className="app">
-        {new_user ? (
-          <SnackBar show timer={6000} className="home">
-            You have successfully signed up to Authors Haven!! Please check your email to verify
-            your account
-          </SnackBar>
-        ) : (
-          ''
-        )}
-        {isFetching && <Loader />}
-        <SearchComponent />
-        <Row class="home">
-          {' '}
-          {article}
-        </Row>
-        {
-          token ? ( <CreateArticleButton />
-            ) : ('')}
-      </div>
-    );
-  }
+		return (
+			<div className="app">
+				{new_user ? (
+					<SnackBar show timer={6000} className="home">
+            You have successfully signed up to Authors Haven!! Please check your
+            email to verify your account
+					</SnackBar>
+				) : (
+					''
+				)}
+				{isFetching && <Loader />}
+				<SearchComponent />
+				<Row class="home"> 
+{' '}
+{article}
+</Row>
+			</div>
+		);
+	}
 }
 
+const mapDispatchToProps = dispatch => bindActionCreators({ getActiveUser, getArticles }, dispatch);
+
 const mapStatetoProps = state => ({
-  home: state.exampleReducer,
-  articles: state.viewArticles,
+	home: state.exampleReducer,
+	articles: state.viewArticles,
 });
 const mapDispatchToProps = dispatch => bindActionCreators({
   getArticles,
