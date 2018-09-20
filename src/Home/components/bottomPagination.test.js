ca99617489046371a4fbcf2b161ac9cadb8f09eb
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2018-09-21 00:43:37 +0300

    [Chore #160011183] Update snapshot test

--- src/Home/components/bottomPagination.test.js
+++ src/Home/components/bottomPagination.test.js
@@ -2,16 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import BottomPagination from './bottomPagination';
 
-describe('<Pagination /> component', () => {
+describe('`<BottomPagination />` component', () => {
 	it('should render without crushing', () => {
 		const snap = shallow(
-			<BottomPagination
-				activePage=""
-				itemsCountPerPage=""
-				totalItemsCount=""
-				pageRangeDisplayed=""
-				onChange={() => ''}
-			/>,
+			<BottomPagination />,
 		);
 		expect(snap).toMatchSnapshot();
 	});
