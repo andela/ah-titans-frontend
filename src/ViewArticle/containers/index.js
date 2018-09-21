ca16c0bf6e0ad520289392658a1121c7aa247f70
Patrick Migot <ptrckmigot@gmail.com>
2018-09-21 12:24:52 +0300

    [Feature #160011181]Remove debug statements

--- src/ViewArticle/containers/index.js
+++ src/ViewArticle/containers/index.js
@@ -36,7 +36,6 @@ class ArticleView extends Component {
 
 	render() {
 		const { article, isFetching } = this.props;
-		console.log(this.props);
 		return (
 			<div>
 				<React.Fragment>{isFetching ? <Loader /> : <ViewArticle article={article} rating={this.state.rating} onRate={this.onRate} />}</React.Fragment>
